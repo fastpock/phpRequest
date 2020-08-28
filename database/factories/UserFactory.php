f0cdd2a22e0d765011d32f6664eb6a736ebf6542
fastpock <slepynin@protonmail.com>
2020-08-28 10:41:36 +0300

    before remove migration create_users_roles_table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,10 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        //'remember_token' => Str::random(10),
+        'active' => rand(0, 1),
+        'role_id' => function () {
+            return factory(App\Role::class)->create()->id;
+        },
     ];
 });
